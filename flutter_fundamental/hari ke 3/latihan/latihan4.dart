eecc403b9574edb910403928658d67988a889ab3
DindaDwi29 <dindadwioct2910@gmail.com>
2022-01-21 08:09:41 +0700

    flutter hari ke 3

---
+++ flutter_fundamental/hari ke 3/latihan/latihan4.dart
@@ -0,0 +1,8 @@
+void main(List<String> arguments) {
+  int nilai1 = 10;
+  int nilai2 = 5;
+  print(nilai1 + nilai2);
+  print(nilai1 - nilai2);
+  print(nilai1 * nilai2);
+  print(nilai1 / nilai2);
+}
